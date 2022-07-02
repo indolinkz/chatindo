61cb0b51a4ac112888a14310789d563eb9edaafb
indolinkz <32545887+indolinkz@users.noreply.github.com>
2022-07-02 16:05:00 +0800

    Add files via upload

--- tiktok.class.php
+++ tiktok.class.php
@@ -5,65 +5,161 @@ class tiktok
     public $enable_proxies = false;
     private $cookie_file = __DIR__ . "/../storage/tiktok-cookie.txt";
 
-    public function http($url, $method = 'POST')
+    public function get($url)
     {
-        $curl = curl_init();
-        curl_setopt_array($curl, array(
-            CURLOPT_URL => 'https://snaptik.app/action-2021.php',
+        $ch = curl_init();
+        $options = array(
+            CURLOPT_URL => $url,
             CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING => '',
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 0,
+            CURLOPT_HEADER => false,
             CURLOPT_FOLLOWLOCATION => true,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => $method,
-            CURLOPT_POSTFIELDS => 'url='.urlencode($url),
             CURLOPT_USERAGENT => _REQUEST_USER_AGENT,
-            CURLOPT_HTTPHEADER => array(
-                'authority: snaptik.app',
-                'sec-ch-ua: "Google Chrome";v="89", "Chromium";v="89", ";Not A Brand";v="99"',
-                'sec-ch-ua-mobile: ?0',
-                'accept: */*',
-                'origin: https://snaptik.app',
-                'sec-fetch-site: same-origin',
-                'sec-fetch-mode: cors',
-                'sec-fetch-dest: empty',
-                'referer: https://snaptik.app/tr',
-                'Content-Type: application/x-www-form-urlencoded',
-            ),
-        ));
-        $response = curl_exec($curl);
-        curl_close($curl);
-        return $response;
+            CURLOPT_ENCODING => "utf-8",
+            CURLOPT_AUTOREFERER => false,
+            CURLOPT_REFERER => 'https://www.tiktok.com/',
+            CURLOPT_CONNECTTIMEOUT => 30,
+            CURLOPT_SSL_VERIFYHOST => false,
+            CURLOPT_SSL_VERIFYPEER => false,
+            CURLOPT_TIMEOUT => 30,
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_COOKIEFILE => $this->cookie_file,
+            CURLOPT_COOKIEJAR => $this->cookie_file,
+        );
+        curl_setopt_array($ch, $options);
+        if (defined('CURLOPT_IPRESOLVE') && defined('CURL_IPRESOLVE_V4')) {
+            curl_setopt($ch, CURLOPT_IPRESOLVE, CURL_IPRESOLVE_V4);
+        }
+        $data = curl_exec($ch);
+        $httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        curl_close($ch);
+        return $data;
+    }
+
+    public function get_redirect_url($url)
+    {
+        $ch = curl_init();
+        $options = array(
+            CURLOPT_URL => $url,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_HEADER => false,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_USERAGENT => _REQUEST_USER_AGENT,
+            CURLOPT_ENCODING => "utf-8",
+            CURLOPT_AUTOREFERER => false,
+            CURLOPT_REFERER => 'https://www.tiktok.com/',
+            CURLOPT_CONNECTTIMEOUT => 30,
+            CURLOPT_SSL_VERIFYHOST => false,
+            CURLOPT_SSL_VERIFYPEER => false,
+            CURLOPT_TIMEOUT => 30,
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_COOKIEFILE => $this->cookie_file,
+            CURLOPT_COOKIEJAR => $this->cookie_file,
+        );
+        curl_setopt_array($ch, $options);
+        if (defined('CURLOPT_IPRESOLVE') && defined('CURL_IPRESOLVE_V4')) {
+            curl_setopt($ch, CURLOPT_IPRESOLVE, CURL_IPRESOLVE_V4);
+        }
+        $data = curl_exec($ch);
+        $httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        $url = curl_getinfo($ch, CURLINFO_EFFECTIVE_URL);
+        curl_close($ch);
+        return $url;
+    }
+
+    private function download_video($url, $file_path)
+    {
+        $fp = fopen($file_path, 'w+');
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_FILE, $fp);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+        curl_setopt($ch, CURLOPT_COOKIEFILE, $this->cookie_file);
+        curl_setopt($ch, CURLOPT_COOKIEJAR, $this->cookie_file);
+        curl_setopt($ch, CURLOPT_REFERER, "https://www.tiktok.com/");
+        curl_setopt($ch, CURLOPT_USERAGENT, _REQUEST_USER_AGENT);
+        curl_exec($ch);
+        curl_close($ch);
+        fclose($fp);
+    }
+
+    private function get_video_key($file_data)
+    {
+        $key = "";
+        preg_match("/vid:([a-zA-Z0-9]+)/", $file_data, $matches);
+        if (isset($matches[1])) {
+            $key = $matches[1];
+        }
+        return $key;
     }
 
     public function media_info($url)
     {
-        $data = $this->http($url);
+        preg_match('/#\/@\w{2,32}\/video\/\d{2,32}/', $url, $matches);
+        if (count($matches) === 1) {
+            $url = "https://www.tiktok.com" . ltrim($matches[0], "#");
+        }
+        $host = str_replace("www.", "", parse_url($url, PHP_URL_HOST));
+        if ($host != "tiktok.com") {
+            $url = unshorten($url, $this->enable_proxies);
+        }
+        preg_match('/https:\/\/www\.tiktok\.com\/@(.*?)\/video\/([0-9]+)/', $url, $matches);
+        if (count($matches) < 3) {
+            return false;
+        }
+        $share_url = "https://www.tiktok.com/node/share/video/@" . $matches[1] . "/" . $matches[2];
+        $share_data = $this->get($share_url);
+        $share_data = json_decode($share_data, true);
+        if (!isset($share_data["itemInfo"]["itemStruct"]["video"]) || empty($share_data["itemInfo"]["itemStruct"]["video"])) {
+            return false;
+        }
         $video["source"] = "tiktok";
-        $video["title"] = get_string_between($data, "<p><span>", "</span></p>");
+        $video["title"] = $share_data["itemInfo"]["itemStruct"]["desc"];
         if (empty($video["title"])) {
-            return false;
+            $video["title"] = $share_data["metaParams"]["title"];
         }
-        $video["thumbnail"] = get_string_between($data, '<img src="', '"');
-        preg_match_all('/\'click_download_mp4_sv\d\'\);" href=\'(.*?)\'/', $data, $matches);
-        if (empty($matches)) {
-            return false;
-        }
-        $i = (int)(count($matches[1]) / 2) - 1;
-        if (!isset($matches[1][$i])) {
-            return false;
-        }
+        $video["thumbnail"] = "https://www.tiktok.com/api/img/?itemId=" . $matches[2] . "&location=0";
+        $video["duration"] = format_seconds($share_data["itemInfo"]["itemStruct"]["video"]["duration"]);
         $video["links"] = array();
-        $size = get_file_size($matches[1][$i], $this->enable_proxies, false);
-        array_push($video["links"], [
-            "url" => $matches[1][$i],
-            "quality" => "HD",
-            "type" => "mp4",
-            "bytes" => $size,
-            "size" => format_size($size),
-            "mute" => false
-        ]);
+        $i = 0;
+        if (!empty($share_data["itemInfo"]["itemStruct"]["video"]["downloadAddr"])) {
+            $track_id = rand(0, 4);
+            $cache_file = __DIR__ . "/../storage/temp/tiktok-" . $track_id . ".mp4";
+            $website_url = json_decode(option("general_settings"), true)["url"];
+            $this->download_video($share_data["itemInfo"]["itemStruct"]["video"]["downloadAddr"], $cache_file);
+            $video_key = $this->get_video_key(file_get_contents($cache_file));
+            $video["links"][$i]["url"] = $website_url . "/system/storage/temp/tiktok-" . $track_id . ".mp4";
+            $video["links"][$i]["type"] = "mp4";
+            $video["links"][$i]["quality"] = $share_data["itemInfo"]["itemStruct"]["video"]["ratio"];
+            $video["links"][$i]["bytes"] = filesize($cache_file);
+            $video["links"][$i]["size"] = format_size($video["links"][$i]["bytes"]);
+            $video["links"][$i]["mute"] = false;
+            $i++;
+            if (!empty($video_key)) {
+                $nwm_video = "https://api2-16-h2.musical.ly/aweme/v1/play/?video_id=$video_key&vr_type=0&is_play_url=1&source=PackSourceEnum_PUBLISH&media_type=4";
+                $nwm_video = $this->get_redirect_url($nwm_video);
+                if (filter_var($nwm_video, FILTER_VALIDATE_URL)) {
+                    $video["links"][$i]["url"] = $nwm_video;
+                    $video["links"][$i]["type"] = "mp4";
+                    $video["links"][$i]["quality"] = $video["links"][$i - 1]["quality"];
+                    $video["links"][$i]["bytes"] = get_file_size($nwm_video, $this->enable_proxies, false);
+                    $video["links"][$i]["size"] = format_size($video["links"][$i]["bytes"]);
+                    $video["links"][$i]["mute"] = false;
+                    $i++;
+                }
+            }
+        }
+        if (!empty($share_data["itemInfo"]["itemStruct"]["music"]["playUrl"])) {
+            $video["links"][$i]["url"] = $share_data["itemInfo"]["itemStruct"]["music"]["playUrl"];
+            $video["links"][$i]["type"] = "mp3";
+            $video["links"][$i]["quality"] = "128 kbps";
+            $video["links"][$i]["bytes"] = get_file_size($video["links"][$i]["url"], $this->enable_proxies, false);
+            $video["links"][$i]["size"] = format_size($video["links"][$i]["bytes"]);
+            $video["links"][$i]["mute"] = false;
+            $i++;
+        }
         return $video;
     }
 }
